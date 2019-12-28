bd07afa56188e78255b166b34c9c27d0553979ba
James Stormes <jstormes@stormes.net>
2019-12-28 16:08:46 -0600

    hello worlds

---
+++ linux_c_code/cmake-build-debug/CMakeFiles/linux_c_code.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linux_c_code.dir/main.c.o"
+  "linux_c_code"
+  "linux_c_code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/linux_c_code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
